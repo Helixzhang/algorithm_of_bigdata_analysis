753fd7896f238d8a2dd8eede5dadcdfa4f388f97
unknown <jyt769199221@gmail.com>
2021-04-20 12:28:40 +0800

    fix RV calculate method

--- RV calculation/RV.py
+++ RV calculation/RV.py
@@ -4,7 +4,7 @@ A = np.matrix([[1,2,3],[2,3,4]])
 # print(np.trace(A*A.I))
 
 def RV(martrixA,martrixB):
-    # return np.trace(martrixA*martrixA.I*martrixB*martrixB.I)/np.sqrt(np.trace(np.power(martrixA*martrixA.I,2))*np.trace(np.power(martrixB*martrixB.I,2)))
-    return np.trace((martrixA.I*martrixB)*(martrixB.I*martrixA))/np.sqrt(np.trace(np.power(martrixA.I*martrixA,2))*np.trace(np.power(martrixB.I*martrixB,2)))
+    return np.trace(martrixA*martrixA.T*martrixB*martrixB.T)/np.sqrt(np.trace(np.power(martrixA*martrixA.T,2))*np.trace(np.power(martrixB*martrixB.T,2)))
+    # return np.trace((martrixA.I*martrixB)*(martrixB.I*martrixA))/np.sqrt(np.trace(np.power(martrixA.I*martrixA,2))*np.trace(np.power(martrixB.I*martrixB,2)))
 
-print(RV(np.matrix([[4,0,6,0],[0,0,0,1]]),np.matrix([[1,1,1],[1,1,1]])))
+print(RV(np.matrix([[4,10,6,8],[0,0,0,1]]),np.matrix([[1,1,1],[1,1,1]])))
